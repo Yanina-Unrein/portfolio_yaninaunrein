f7513f6a3c5440cf9a290a7c9f93f300a8f09f01
Yanina-Unrein <unrein.syanina@gmail.com>
2024-08-04 14:05:54 -0300

    componente proyecto con animaciones y correcciones a otros componentes

---
+++ astro-portfolio/src/data/projects_colaborate.js
@@ -0,0 +1,8 @@
+export const PROJECTSC = [
+    {
+        image: '/cardsImg/Igro/Igro-Banner.png',
+        title: 'Banner del Hero de Igro',
+        description: 'Banner interactivo realizado en el CMS HubSpot con HTML y CSS. Permite al cliente cambiar las imágenes y el texto de cada ítem y al hacer hover, cambia dinámicamente tanto el texto como la imágen.',
+        link: 'https://www.goigro.com/es/',        
+    },
+];
