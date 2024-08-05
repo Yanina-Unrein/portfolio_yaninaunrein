d60b3f156b33ecaf11d5ada4e2138727a21c9e74
Yanina-Unrein <unrein.syanina@gmail.com>
2024-08-05 19:29:28 -0300

    modificaciones varias en componentes

--- astro-portfolio/src/data/experience.js
+++ astro-portfolio/src/data/experience.js
@@ -1,12 +1,17 @@
 export const EXPERIENCE = [
     {
+        date: 'Marzo -2024',
+        title: 'Freelance - colaboración de maquetación',
+        description: 'Hero slider con video de fondo realizado en Wordpress con el constructor Divi, totalmente responsivo.',
+    },
+    {
         date: 'Enero 2024 - actualidad',
-        title: 'Programming Intern',
+        title: 'Publissoft - Programming Intern',
         description: 'Gestiono la integración de contenido y la publicación de artículos en diversos CMS como WordPress, Shopify, Wix, Prestashop y CPanel. Mi trabajo incluye la creación de páginas web, la optimización de contenido para mejorar la visibilidad y la experiencia del usuario, y la colaboración con equipos multidisciplinarios para asegurar que el contenido esté alineado con las estrategias y objetivos del negocio. Mantengo los sitios web actualizados y relevantes, adaptando el contenido a las necesidades cambiantes.',
     },
     {
-        date: 'Enero 2024',
-        title: 'Freelance colaboracion de maquetación',
+        date: 'Octubre 2023',
+        title: 'Freelance - colaboración de maquetación',
         description: 'Banner interactivo realizado en el CMS HubSpot con HTML y CSS. Permite al cliente cambiar las imágenes y el texto de cada ítem y al hacer hover, cambia dinámicamente tanto el texto como la imágen.',
     },
 ];
--- astro-portfolio/src/data/projects_colaborate.js
+++ astro-portfolio/src/data/projects_colaborate.js
@@ -1,5 +1,11 @@
 export const PROJECTSC = [
     {
+        image: '/cardsImg/ilkAbogados/IlkAbogadosHeader.jpg',
+        title: 'Hero Slider de ILK Abogados Argentina',
+        description: 'Hero slider con video de fondo realizado en Wordpress con el constructor Divi, totalmente responsivo.',
+        link: 'https://ilk.com.ar/',        
+    },
+    {
         image: '/cardsImg/Igro/Igro-Banner.png',
         title: 'Banner del Hero de Igro',
         description: 'Banner interactivo realizado en el CMS HubSpot con HTML y CSS. Permite al cliente cambiar las imágenes y el texto de cada ítem y al hacer hover, cambia dinámicamente tanto el texto como la imágen.',
