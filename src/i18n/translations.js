639dd86c56d69791c468a9181d0f382563a8660e
Yanina-Unrein <unrein.syanina@gmail.com>
2024-10-04 00:40:28 -0300

    button traduccion de idioma maquetacion

---
+++ src/i18n/translations.js
@@ -0,0 +1,114 @@
+export const translations = {
+    es: {
+      navbar: {
+        home: "Inicio",
+        about: "Sobre Mi",
+        experience: "Experiencia",
+        projects: "Proyectos",
+        technologies: "Tecnologías",
+        contact: "Contacto",
+      },
+      footer: {
+        home: "Inicio",
+        projects: "Proyectos",
+        contact: "Contacto",
+        copyright_1: "Página hecha con el ❤️",
+      },
+      hero: {
+        text: "Hola, soy Yanina Unrein",
+      },
+      aboutMe: {
+        title: "Sobre Mí",
+        subTitle: "Hola, soy Yanina Unrein",
+        description_1: "Soy una apasionada por la programación, los videojuegos, mangas, animes y gatos. Soy proactiva, comprometida, autodidacta y me gusta siempre dar lo mejor de mí en todo, por más simple o compleja que se pueda tornar la situación.",
+        description_2: "En julio del 2024 me recibí como",
+        description_3: "Técnica en Desarrollo Web y Aplicaciones Digitales",
+        description_4: "en el Instituto Superior Politécnico Córdoba. Actualmente me encuentro estudiando la",
+        description_5: "Tecnicatura en Desarrollo Web",
+        description_6: "en la Universidad de Entre Ríos en mi segundo año y en continuo aprendizaje con cursos, bootcamp o de forma autodidacta.",
+        cv_text: "Si estás interesado en conocer más sobre mí, puedes descargar mi Curriculum Vitae a continuación:",
+        btn_dowload: "Descargar CV",
+      },
+      projects: {
+        title: "Proyectos",
+        subtitle_1: "Trabajos seleccionados",
+        subtitle_2: "Colaboraciones",
+      },
+      technologies: {
+        title: "Tecnologías",
+        description_1: "Mi área principal de especialización es el desarrollo Frontend pero mi formación es FullStack.",
+        description_2: "HTML, CSS, JS, creación de aplicaciones web pequeñas y medianas con Angular o React, complementos personalizados, funciones, animaciones y codificación de diseños interactivos. También tengo experiencia como desarrolladora con CMS como WordPress, Shopify, CPanel, Wix.",
+        description_3: "Visita mi para más detalles.",
+      },
+      experience: {
+        title: "Experiencia",
+        description: "Aquí están algunos de mis roles anteriores y lo que he aprendido...",
+      },
+      contact: {
+        title: "Contáctame",        
+      },
+      contactForm: {
+        name: "Nombre y Apellido", 
+        mail: "Correo Electrónico",       
+        message: "Mensaje",
+        btn_submit: "Enviar",
+        message_submit: "Mensaje enviado, me pondré en contacto contigo apenas lo lea. Muchas Gracias!"
+      },
+    },
+    en: {
+        navbar: {
+          home: "Home",
+          about: "About Me",
+          experience: "Experience",
+          projects: "Projects",
+          technologies: "Technologies",
+          contact: "Contact",
+        },
+        footer: {
+          home: "Home",
+          projects: "Projects",
+          contact: "Contact",
+          copyright_1: "Page made with ❤️",
+        },
+        hero: {
+          text: "Hello, I'm Yanina Unrein",
+        },
+        aboutMe: {
+          title: "About Me",
+          subTitle: "Hello, I'm Yanina Unrein",
+          description_1: "I am passionate about programming, video games, manga, anime, and cats. I am proactive, committed, self-taught, and I always strive to do my best in everything, no matter how simple or complex the situation may be.",
+          description_2: "In July 2024, I graduated as",
+          description_3: "Technician in Web Development and Digital Applications",
+          description_4: "from the Instituto Superior Politécnico Córdoba. I am currently studying",
+          description_5: "Web Development Technology",
+          description_6: "at the Universidad de Entre Ríos in my second year, and I am continuously learning through courses, bootcamps, or self-taught methods.",
+          cv_text: "If you are interested in knowing more about me, you can download my Curriculum Vitae below:",
+          btn_dowload: "Download CV",
+        },
+        projects: {
+          title: "Projects",
+          subtitle_1: "Selected Works",
+          subtitle_2: "Collaborations",
+        },
+        technologies: {
+          title: "Technologies",
+          description_1: "My main area of expertise is Frontend development, but my training is FullStack.",
+          description_2: "HTML, CSS, JS, creating small and medium web applications with Angular or React, custom plugins, functions, animations, and coding interactive designs. I also have experience as a developer with CMS such as WordPress, Shopify, CPanel, Wix.",
+          description_3: "Visit my for more details.",
+        },
+        experience: {
+          title: "Experience",
+          description: "Here are some of my previous roles and what I have learned...",
+        },
+        contact: {
+          title: "Contact Me",
+        },
+        contactForm: {
+          name: "Full Name",
+          mail: "Email Address",
+          message: "Message",
+          btn_submit: "Send",
+          message_submit: "Message sent, I will contact you as soon as I read it. Thank you very much!",
+        },
+      },
+    };
